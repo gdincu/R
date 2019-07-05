54406f8e423c5d1f98036f17b732473c89d3278c
Ionut Dincu <gabrieldincu@outlook.com>
2019-07-05 23:48:27 +0300

    Add files via upload

---
+++ 1_Basic_Syntax.r
@@ -0,0 +1,11 @@
+#CTRL + L clears the screen
+
+#Display the workspace (collection of objects currently stored)
+- objects()
+- ls()
+- ls.str()
+
+#Remove objects
+- rm(x, y, z, ink, junk, temp, foo, bar)
+- rm(list=ls())
+
