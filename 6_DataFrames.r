f74ba4632da5af856de21f836a1b7cc0d5781bf2
Ionut Dincu <gabrieldincu@outlook.com>
2019-07-16 18:12:44 +0100

    Add files via upload

--- 6_DataFrames.r
+++ 6_DataFrames.r
@@ -8,19 +8,22 @@ d$Grade.Name[is.na(d$Grade.Name)] <- NA
 #Remove values of a column based on their value
 d <- d[!(d$Grade.Name == "Exec"),]
 
-#Delete all objects
-rm(list=ls())
-
 #Selecting rows or columns
 d[[5]]	- Returns one column—specifically, the nth column of dfrm
-d[3,]		- Returns the third Row
+d[3,]		- Returns the third Row as a dataframe
 d[,5] 	- Fifth Column
-d[c(8,9)]	- Returns a data frame build from the columns 8 and 9
+d <- d[c(8,9)]	- Returns a data frame build from the columns 8 and 9
 ###To retain the dimensions of the initial data frame use drop=FALSE
-eg. mat[1,,drop=FALSE]
+eg. d[2,,drop=FALSE]
 
+#Rename columns
+colnames(d)[1] <- "Test"
 
+#Remove columns
+d[[3]] <- NULL
 
+#Export data frame to CSV file
+write.csv(d, file.choose(), row.names = FALSE)
 
-
-
+#Delete all objects
+rm(list=ls())
